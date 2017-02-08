629de7149f7e285f25914e63f6892a71630c28c5
Beth Cummings <bethios@gmail.com>
2017-02-08 12:34:32 -0600

    DOM events checkpoint

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,18 +1,29 @@
-var animatePoints = function ( ) {
-              
-    var points = document.getElementsByClassName('point');
-    
-    var revealPoint = function() {
-        points[i].style.opacity = 1; 
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform= "scaleX(1) translateY(0)";
-        points[i].style.WebKitTransform = "scaleX(1) translateY(0)"
+var pointsArray = document.getElementsByClassName('point');
+
+var revealPoint = function(point) {
+        point.style.opacity = 1; 
+        point.style.transform = "scaleX(1) translateY(0)";
+        point.style.msTransform= "scaleX(1) translateY(0)";
+        point.style.WebKitTransform = "scaleX(1) translateY(0)"
     };
-    
-    for(var i =0; i< points.length; i++){
-        revealPoint(i);
-    }
+
+var animatePoints = function(points) {
+    forEach(pointsArray, revealPoint)
                 
 };
 
-animatePoints();
+window.onload = function(){
+    if(window.innerHeight > 950){
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight +200;
+    
+    window.addEventListener('scroll', function(event){
+        if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+            animatePoints(pointsArray);
+        }
+    });
+}
