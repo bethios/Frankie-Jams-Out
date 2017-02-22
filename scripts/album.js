e1645d6c7d92b552ad4299e36cabdbb3f22f4d44
Beth Cummings <bethios@gmail.com>
2017-02-22 08:15:35 -0600

    added buzz library features and mp3 files

--- scripts/album.js
+++ scripts/album.js
@@ -1,6 +1,22 @@
 var setSong = function(songNumber){
+    if(currentSoundFile){
+        currentSoundFile.stop();
+    }
+
     currentlyPlayingSongNumber = Number(songNumber);
     currentSongFromAlbum = currentAlbum.songs[songNumber -1];
+
+    currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+        formats: ['mp3'],
+        preload: true
+    });
+    setVolume(currentVolume);
+};
+
+var setVolume = function(volume){
+    if(currentSoundFile){
+        currentSoundFile.setVolume(volume);
+    }
 };
 
 var getSongNumberCell = function(number){
@@ -30,11 +46,18 @@ var createSongRow = function(songNumber, songName, songLength){
         if(currentlyPlayingSongNumber !== songNumber){
             $(this).html(pauseButtonTemplate);
             setSong(songNumber);
+            currentSoundFile.play();
             updatePlayerBarSong();
         }else if(currentlyPlayingSongNumber === songNumber){
-            $(this).html(playButtonTemplate);
-            $('.main-controls .play-pause').html(playerBarPlayButton);
-            setSong(null);
+            if(currentSoundFile.isPaused() === true){
+                $(this).html(pauseButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPauseButton);
+                currentSoundFile.play();
+            }else{
+                $(this).html(playButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPlayButton);
+                currentSoundFile.pause();
+            }
         }
     };
 
@@ -120,6 +143,7 @@ var skipSong = function(directionToSkip){
     }
 
     setSong(nextSongNumber);
+    currentSoundFile.play();
     updatePlayerBarSong();
 
     var $previousSongNumberCell = getSongNumberCell(currentlyPlayingSongNumber);
@@ -142,6 +166,8 @@ var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause">
 var currentAlbum = null;
 var currentlyPlayingSongNumber = null;
 var currentSongFromAlbum = null;
+var currentSoundFile = null;
+var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
